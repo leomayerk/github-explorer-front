89ec9a09d0feb22d333b43fa83a3cd08e73f2eaf
leomayerk <leomayerk@hotmail.com>
2020-12-05 15:05:06 -0300

    Save in storage

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -37,7 +37,7 @@ const Dashboard: React.FC = () => {
 		event.preventDefault();
 
 		if (!newRepo) {
-			return setInputError('Digite o autor/nome do repositório')
+			return setInputError('Digite o autor/nome do repositório');
 		}
 
 		try {
